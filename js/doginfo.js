43b40bd3e8ae913f86679e888dee38abb006a879
laflad <laflad04@gmail.com>
2022-03-25 18:09:33 -0700

    add js file

---
+++ js/doginfo.js
@@ -0,0 +1,47 @@
+function dogInfoMurphy() {
+    alert(`Name: Murphy\nBreed: Not a cat\nAdoption Fee: $123.45`);
+}
+
+function dogInfoPoppy() {
+    alert(`Name: Poppy\nBreed: Probably a cat\nAdoption Fee: $123.45`);
+}
+
+function dogInfoJack() {
+    alert(`Name: Jack\nBreed: Flappy  ears\nAdoption Fee: $123.45`);
+}
+
+function dogInfoDuffy() {
+    alert(`Name: Duffy\nBreed: Look at those eyes\nAdoption Fee: $123.45`);
+}
+
+function dogInfoLucas() {
+    alert(`Name: Lucas\nBreed: Happy puppy\nAdoption Fee: $123.45`);
+}
+
+function dogInfoJake() {
+    alert(`Name: Jake\nBreed: Golden personality\nAdoption Fee: $123.45`);
+}
+
+function dogInfoAngus() {
+    alert(`Name: Angus\nBreed: Squishy face\nAdoption Fee: $123.45`);
+}
+
+function dogInfoViolet() {
+    alert(`Name: Violet\nBreed: Too cute to be real\nAdoption Fee: $123.45`);
+}
+
+function dogInfoPiper() {
+    alert(`Name: Piper\nBreed: Hiking pro\nAdoption Fee: $123.45`);
+}
+
+function dogInfoMaximus() {
+    alert(`Name: Maximus\nBreed: Serious Serge\nAdoption Fee: $123.45`);
+}
+
+function dogInfoLuna() {
+    alert(`Name: Luna\nBreed: Totally just did something naughty\nAdoption Fee: $123.45`);
+}
+
+function dogInfoStella() {
+    alert(`Name: Stella\nBreed: Definitely a cat\nAdoption Fee: $123.45`);
+}
